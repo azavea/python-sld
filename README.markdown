72589356b0e847dfb12f5dbe3f48b10201aea60b
David Zwarg <dzwarg@azavea.com>
2011-12-02 16:19:38 -0500

    Added setup script for pypi distribution.

--- README.markdown
+++ README.markdown
@@ -1,4 +1,22 @@
-django-sld
+python-sld
 ==========
 
-A django library for reading, writing, and manipulating SLD files.
+A python library for reading, writing, and manipulating SLD files.
+
+Requirements
+============
+
+lxml is required for XML parsing and XML Schema validation. This requirement
+is listed in requirements.txt, and may be installed using pip with this
+command:
+
+    > sudo pip install -r requirements.txt
+
+Installation
+============
+
+    > easy_install python-sld
+
+    OR
+
+    > pip install python-sld
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+Setup script for python-sld.
+
+License
+=======
+Copyright 2011 David Zwarg <dzwarg@azavea.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-sld",
+    version = "1.0.0",
+    author = "David Zwarg",
+    author_email = "dzwarg@azavea.com",
+    description = ("A simple python library that enables dynamic SLD creation and manipulation."),
+    license = "Apache 2.0",
+    keywords = "ogc sld geo geoserver mapserver osgeo",
+    url = "http://github.com/dzwarg/python-sld/",
+    packages = ["sld"],
+    long_description = read('README.markdown'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: GIS"
+    ]
+)
